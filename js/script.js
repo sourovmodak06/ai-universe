ef0e93c05f68458d6176a8c88d58f383d6e44d81
sourovmodak06 <sourovmodak650@gmail.com>
2023-03-03 00:01:58 +0600

    display card

--- js/script.js
+++ js/script.js
@@ -20,23 +20,42 @@ const displayData = (hubData) => {
     const cardData = document.createElement("div");
     cardData.innerHTML = `
     <div class="card w-96 glass">
-        <figure><img src="https://picsum.photos/200" alt="Card Images"/></figure>
+        <figure><img class="h-48 w-full p-2 rounded-2xl" src="${element.image}" alt="Card Images"/></figure>
         <div class="card-body">
-            <h2 class="card-title">Life hack</h2>
-            <p>How to park your car at your garage?</p>
-            <div class="card-actions justify-end">
-                <button class="btn btn-primary">Learn now!</button>
+            <h2 class="card-title">Features</h2>
+            <ol class="list-decimal ml-5">
+                <li>${element.features[0] ? element.features[0]: 'No data Found'}</li>
+                <li>${element.features[1] ? element.features[1]: 'No data Found'}</li>
+                <li>${element.features[2] ? element.features[2]: 'No data Found'}</li>
+            </ol>
+            <hr>
+            <div class="flex justify-between items-center">
+                <div>
+                    <h2 class="card-title my-2">${element.name}</h2>
+                    <div class="text-gray-400">
+                        <span><i class="fa fa-calendar"></i></span> ${element.published_in}
+                    </div>
+                </div>
+                <div class="bg-red-50 hover:bg-red-300 cursor-pointer h-10 w-10 rounded-full flex items-center justify-center">
+                    <i class="fa fa-arrow-right"></i>
+                </div>
             </div>
         </div>
     </div>
     `;
     cardContainer.appendChild(cardData);
+    console.log(element.name);
 });
 };
+
+
 
 loadApiData();
 
 
 /*
-
+<p>How to park your car at your garage?</p>
+            <div class="card-actions justify-end">
+                <button class="btn btn-primary">Learn now!</button>
+            </div>
 */
